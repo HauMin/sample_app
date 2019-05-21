5821a0740198b0f246ab2fe1fc53c97fb757777a
HauMin <haumin1201@gmail.com>
2019-05-21 15:33:27 +0700

    chapter3 : Mostly static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = I18n.t " base-main"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
