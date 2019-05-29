8225019ee58cc5689207c5e2b45ad0bfd6686f71
HauMin <haumin1201@gmail.com>
2019-05-29 18:11:49 +0700

    Chapter 13: User microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  scope :newest, -> order(created_at: :desc)
+  scope :newest, ->{order created_at: :desc}
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: Settings.content_length}
