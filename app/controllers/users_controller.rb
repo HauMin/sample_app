216593c4a878df497dcd0d338645aab1f2c0e2e4
HauMin <haumin1201@gmail.com>
2019-05-29 09:20:22 +0700

    Chapter 7: Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,28 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    return @user if @user = User.find_by(id: params[:id])
+    flash[:danger] = I18n.t "user_not_found"
+    redirect_to root_url
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = I18n.t "wcttsa"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email,
+      :password, :password_confirmation
+  end
 end
